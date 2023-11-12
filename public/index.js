d3f1e3c929737bfd31b6d3d7765a287fc12c47c8
BanJadrov <janbadrov5@gmail.com>
2023-11-13 00:49:17 +0100

    xss enabled

--- public/index.js
+++ public/index.js
@@ -1,23 +1,11 @@
 const filePath = 'passwords/passwords.txt';
-
 function onSubmit() {
-    let email = document.getElementById('email').value;
-    let password = document.getElementById('password').value;
-    let tekst = email + ":" + password;
-    fs.appendFile(filePath, `${tekst}\n`, (err) => {
-        if (err) {
-            res.status(500).send('Greška pri dodavanju u datoteku');
-        } else {
-            res.send('Podaci dodani u datoteku');
-        }
-    });
-    console.log("Registered: " + email);
     document.getElementById("username").innerText = email.split('@')[0];
     document.getElementById("registrationSection").style.display = "none";
     document.getElementById("aboutMeSection").style.display = "block";
 }
 
-function onSubmitAboutMe() {
+function onSubmitAboutMe(req, res) {
     let aboutMe = document.getElementById('aboutMe').value;
     console.log("About Me submitted: " + aboutMe);
     document.getElementById("aboutMeText").innerText = aboutMe;
--- server.js
+++ server.js
@@ -1,13 +1,57 @@
 const express = require('express');
 const fs = require('fs');
 const cors = require('cors');
+const path = require('path');
 const app = express();
-const port = 3000;
+const port = 5000;
 
-// Putanja do .txt datoteke
-const filePath = 'passwords/passwords.txt';
+filePath = 'passwords/passwords.txt';
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname)));
+paths();
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.listen(port, () => {
     console.log(`Server sluša na portu ${port}`);
 });
+
+function paths() {
+    app.post('/register', (req, res) => {
+        console.log(req.body.email)
+        let email = req.body.email;
+        let password = req.body.password;
+        let text = email + ":" + password + "\n";
+        fs.appendFile(filePath, text, (err) => {
+            if (err) {
+                res.status(500).send(err);
+            } else {
+                res.redirect('/aboutMe');
+            }
+        });
+    });
+    app.get("/aboutMe", (req, res) => {
+        putanja = path.join(__dirname, 'public/html', 'aboutMe.html');
+        console.log(putanja);
+        res.sendFile(putanja);
+    });
+    app.post("/aboutMe", (req, res) => {
+        let aboutMe = req.body.aboutMe;
+        let vulnerable = req.body.vulnerable;
+        res.redirect(`/aboutMePage?aboutMe=${aboutMe}&vulnerable=${vulnerable}`);
+    });
+    app.get("/aboutMePage", (req, res) => {
+        let aboutMe = req.query.aboutMe;
+        let vulnerable = req.query.vulnerable;
+        console.log(vulnerable);
+        if(vulnerable == 'on')
+            res.send(`About Me: ${aboutMe}`);
+        else
+            if(aboutMe.includes('<script>') || aboutMe.includes('</script>')){
+                res.status(403);
+                res.send(`Nedopušteno`);
+            }
+    });
+}
