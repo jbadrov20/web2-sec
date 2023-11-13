0bfaaa92d34cd9b2ca0321eaf4628982642fc6c5
BanJadrov <janbadrov5@gmail.com>
2023-11-13 01:59:12 +0100

    Enable Sensitive Data Exposure

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const cors = require('cors');
 const path = require('path');
 const app = express();
 const port = 5000;
-
+var sde = false;
 filePath = 'passwords/passwords.txt';
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname, 'public')));
 paths();
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
@@ -20,13 +20,16 @@ app.listen(port, () => {
 
 function paths() {
     app.post('/register', (req, res) => {
-        console.log(req.body.email)
         let email = req.body.email;
         let password = req.body.password;
         let text = email + ":" + password + "\n";
+        let vulnerable = req.body.checkbox;
         fs.appendFile(filePath, text, (err) => {
             if (err) {
                 res.status(500).send(err);
+            } else if (vulnerable == 'on') {
+                sde = true;
+                res.redirect('/aboutMe');
             } else {
                 res.redirect('/aboutMe');
             }
@@ -34,7 +37,6 @@ function paths() {
     });
     app.get("/aboutMe", (req, res) => {
         putanja = path.join(__dirname, 'public/html', 'aboutMe.html');
-        console.log(putanja);
         res.sendFile(putanja);
     });
     app.post("/aboutMe", (req, res) => {
@@ -45,13 +47,28 @@ function paths() {
     app.get("/aboutMePage", (req, res) => {
         let aboutMe = req.query.aboutMe;
         let vulnerable = req.query.vulnerable;
-        console.log(vulnerable);
-        if(vulnerable == 'on')
+        if (vulnerable == 'on')
             res.send(`About Me: ${aboutMe}`);
         else
-            if(aboutMe.includes('<script>') || aboutMe.includes('</script>')){
+            if (aboutMe.includes('<script>') || aboutMe.includes('</script>')) {
                 res.status(403);
                 res.send(`Nedopušteno`);
             }
     });
+    app.get("/Robots.txt", (req, res) => {
+        if (sde == false) {
+            res.status(403);
+            res.send(`Nedopušteno`);
+        } else {
+            res.sendFile(path.join(__dirname, 'Robots.txt'));
+        }
+    })
+    app.get("/passwords/passwords.txt", (req, res) => {
+        if (sde == false) {
+            res.status(403);
+            res.send(`Nedopušteno`);
+        } else {
+            res.sendFile(path.join(__dirname, 'passwords', 'passwords.txt'));
+        }
+    })
 }
