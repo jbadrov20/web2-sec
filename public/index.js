1878159fe9a6b927fb21de43286c440c65d84dd9
jbadrov20 <janbadrov5@gmail.com>
2024-11-11 02:14:21 +0100

    Added Broken Access control

--- public/index.js
+++ public/index.js
@@ -1,4 +1,3 @@
-const filePath = 'passwords/passwords.txt';
 function onSubmit() {
     document.getElementById("username").innerText = email.split('@')[0];
     document.getElementById("registrationSection").style.display = "none";
@@ -8,7 +7,29 @@ function onSubmit() {
 function onSubmitAboutMe(req, res) {
     let aboutMe = document.getElementById('aboutMe').value;
     console.log("About Me submitted: " + aboutMe);
-    document.getElementById("aboutMeText").innerText = aboutMe;
-    document.getElementById("aboutMeSection").style.display = "none";
-    document.getElementById("aboutMeContent").style.display = "block";
+    document.getElementById("aboutMeInfo").innerText = aboutMe;
+    document.getElementById("aboutMeInfo").style.display = "block";
 }
+
+async function fetchUserData(event) {
+    event.preventDefault();
+  
+    const pass = document.getElementById("pass").value; 
+    const username = document.getElementById("username").value;
+    const vulnerability2 = document.getElementById("vulnerability2").checked ? "yes" : "no";
+    const userInfo = document.getElementById("userInfo");
+  
+    try {
+      const response = await fetch(`/user/profile?username=${username}&pass=${pass}&vulnerability2=${vulnerability2}`);
+      
+      if (response.ok) {
+        const data = await response.json();
+        userInfo.textContent = `email: ${data.email}, password: ${data.password}`;
+      } else {
+        const error = await response.json();
+        userInfo.textContent = error.error;
+      }
+    } catch (error) {
+      userInfo.textContent = "Greška pri povezivanju sa serverom.";
+    }
+  }
--- server.js
+++ server.js
@@ -1,74 +1,117 @@
-const express = require('express');
-const fs = require('fs');
-const cors = require('cors');
-const path = require('path');
+const express = require("express");
+const fs = require("fs").promises;
+const cors = require("cors");
+const path = require("path");
 const app = express();
 const port = 5000;
 var sde = false;
-filePath = 'passwords/passwords.txt';
+const data = require("./passwords/passwords.json");
+const filePath = "./passwords/passwords.json";
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 paths();
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(port, () => {
-    console.log(`Server sluša na portu ${port}`);
+  console.log(`Server sluša na portu ${port}`);
 });
 
 function paths() {
-    app.post('/register', (req, res) => {
-        let email = req.body.email;
-        let password = req.body.password;
-        let text = email + ":" + password + "\n";
-        let vulnerable = req.body.checkbox;
-        fs.appendFile(filePath, text, (err) => {
+  app.post("/register", async (req, res) => {
+    let email = req.body.email;
+    let password = req.body.password;
+    let vulnerable = req.body.checkbox;
+    console.log(email)
+    try {
+        const data = await fs.readFile(filePath, "utf8");
+        console.log(data);
+        const users = JSON.parse(data);
+
+        const user = users.find((u) => u.email === email);
+        if (user) {
+          res.status(500).send("E-mail already registered");
+          return;
+        } else if (vulnerable == "on") {
+          users.push({ email, password });
+          fs.writeFile(filePath, JSON.stringify(users, null, 2), (err) => {
             if (err) {
-                res.status(500).send(err);
-            } else if (vulnerable == 'on') {
-                sde = true;
-                res.redirect('/aboutMe');
+              console.error("Error writing file:", err);
             } else {
-                res.redirect('/aboutMe');
+              console.log("User added successfully!");
             }
-        });
-    });
-    app.get("/aboutMe", (req, res) => {
-        putanja = path.join(__dirname, 'public/html', 'aboutMe.html');
-        res.sendFile(putanja);
-    });
-    app.post("/aboutMe", (req, res) => {
-        let aboutMe = req.body.aboutMe;
-        let vulnerable = req.body.vulnerable;
-        res.redirect(`/aboutMePage?aboutMe=${aboutMe}&vulnerable=${vulnerable}`);
-    });
-    app.get("/aboutMePage", (req, res) => {
-        let aboutMe = req.query.aboutMe;
-        let vulnerable = req.query.vulnerable;
-        if (vulnerable == 'on')
-            res.send(`About Me: ${aboutMe}`);
-        else
-            if (aboutMe.includes('<script>') || aboutMe.includes('</script>')) {
-                res.status(403);
-                res.send(`Nedopušteno`);
+          });
+          sde = true;
+          res.redirect("/aboutMe");
+        } else {
+          users.push({ email, password });
+          fs.writeFile(filePath, JSON.stringify(users, null, 2), (err) => {
+            if (err) {
+              console.error("Error writing file:", err);
+            } else {
+              console.log("User added successfully!");
             }
-    });
-    app.get("/Robots.txt", (req, res) => {
-        if (sde == false) {
-            res.status(403);
-            res.send(`Nedopušteno`);
-        } else {
-            res.sendFile(path.join(__dirname, 'Robots.txt'));
+          });
+          res.redirect("/aboutMe");
         }
-    })
-    app.get("/passwords/passwords.txt", (req, res) => {
-        if (sde == false) {
-            res.status(403);
-            res.send(`Nedopušteno`);
+    } catch (err) {
+      console.error("Error reading file:", err);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  });
+  app.get("/aboutMe", (req, res) => {
+    putanja = path.join(__dirname, "public/html", "aboutMe.html");
+    res.sendFile(putanja);
+  });
+  app.post("/aboutMe", (req, res) => {
+    let aboutMe = req.body.aboutMe;
+    let vulnerable = req.body.vulnerable;
+    res.redirect(`/aboutMePage?aboutMe=${aboutMe}&vulnerable=${vulnerable}`);
+  });
+  app.get("/aboutMePage", (req, res) => {
+    let aboutMe = req.query.aboutMe;
+    let vulnerable = req.query.vulnerable;
+    if (vulnerable == "on") res.send(`About Me: ${aboutMe}`);
+    else if (aboutMe.includes("<script>") || aboutMe.includes("</script>")) {
+      res.status(403);
+      res.send(`Nedopušteno`);
+    }
+  });
+
+  app.get("/user/profile", async (req, res) => {
+    const userEmail = req.query.username;
+    const pass = req.query.pass;
+    const vulnerable = req.query.vulnerability2;
+    console.log(`Received - Email: ${userEmail}, Password: ${pass}, Vulnerability: ${vulnerable}`);
+  
+
+    if (pass !== "admin" && vulnerable !== "yes") {
+      return res.status(403).json({ error: "Access denied" });
+    }
+
+    if (pass == "admin" || vulnerable === "yes") {
+      try {
+        const data = await fs.readFile(filePath, "utf8");
+        console.log(data);
+        const users = JSON.parse(data);
+
+        const user = users.find((u) => u.email === userEmail);
+
+        if (user) {
+          res.json(user);
         } else {
-            res.sendFile(path.join(__dirname, 'passwords', 'passwords.txt'));
+          res.status(404).json({ error: "User not found" });
         }
-    })
+      } catch (err) {
+        console.error("Error reading users file:", err);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    }
+  });
+}
+
+function emailExists(email) {
+  return data.find((u) => u.email === email);
 }
